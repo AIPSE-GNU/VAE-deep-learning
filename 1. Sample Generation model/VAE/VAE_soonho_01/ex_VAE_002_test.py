9b101b6ea3fe49efae3e4e1dd780cbdb104a2ead
AIPSE-GNU <s.hwangbo@gnu.ac.kr>
2021-10-19 17:25:18 +0900

    sample generation-2

---
+++ 1. Sample Generation model/VAE/VAE_soonho_01/ex_VAE_002_test.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from scipy.stats import norm
+from utils.loaders import load_mnist, load_model
+from models.VAE_CONV_D1 import VariationalAutoencoder
+import pandas as pd
+
+# 실행 매개변수
+SECTION = 'vae_conv_d1'
+RUN_ID = '0001'
+DATA_NAME = 'res'
+RUN_FOLDER = 'run/{}/'.format(SECTION)
+RUN_FOLDER += '_'.join([RUN_ID, DATA_NAME])
+
+# 훈련된 모델이 저장된 폴더에서 훈련된 모델 호출
+VAE = load_model(VariationalAutoencoder, RUN_FOLDER)
+
+from numpy import mean
+from numpy import std
+import numpy as np
+from numpy import dstack
+from pandas import read_csv
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
+from keras.utils import to_categorical
+
+# 훈련에 사용된 scaler를 다시 호출하기 위한 프로세스
+from ex_load_data_002 import load_dataset
+x_train = load_dataset()
+scaler = MinMaxScaler(feature_range=(0, 1))
+x_train = scaler.fit_transform(x_train.reshape(x_train.shape[0], -1)).reshape(x_train.shape)
+
+# 생성하고자 하는 sample 개수(n_to_show) 설정
+n_to_show = 365000
+znew = np.random.normal(size=(n_to_show, VAE.z_dim))
+reconst_test = VAE.decoder.predict(np.array(znew))
+
+# extracted features를 활용하여 생선한 sample들을 원래 구조로 전환(inverse_transform)
+# 개별 파일로 분리하여 해당 폴더에 sample들을 저장
+x_test = scaler.inverse_transform(reconst_test.reshape(reconst_test.shape[0], -1)).reshape(reconst_test.shape)
+x_test_jeju_solar = x_test[:,:,0]
+x_test_jeju_wind = x_test[:,:,1]
+x_test_land_solar = x_test[:,:,2]
+x_test_land_wind = x_test[:,:,3]
+generative_test_demand = pd.DataFrame(x_test_jeju_solar)
+generative_test_demand.to_csv("./RES/vae_generative_test_jeju_solar_01.csv", header=False, index=False)
+generative_test_solar = pd.DataFrame(x_test_jeju_wind)
+generative_test_solar.to_csv("./RES/vae_generative_test_jeju_wind_01.csv", header=False, index=False)
+generative_test_solar = pd.DataFrame(x_test_land_wind)
+generative_test_solar.to_csv("./RES/vae_generative_test_land_solar_01.csv", header=False, index=False)
+generative_test_solar = pd.DataFrame(x_test_land_wind)
+generative_test_solar.to_csv("./RES/vae_generative_test_land_wind_01.csv", header=False, index=False)
+
---
+++ 1. Sample Generation model/VAE/VAE_soonho_01/ex_VAE_002_train.py
@@ -0,0 +1,78 @@
+# cnn model
+from numpy import mean
+from numpy import std
+import numpy as np
+from numpy import dstack
+from pandas import read_csv
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+
+# 데이터 호출 (sampling의 경우 test data는 사용되지 않음)
+from ex_load_data_002 import load_dataset
+# x_train, x_test = load_dataset()
+x_train = load_dataset()
+
+# 데이터 스케일링
+scaler = MinMaxScaler(feature_range=(0, 1))
+x_train = scaler.fit_transform(x_train.reshape(x_train.shape[0], -1)).reshape(x_train.shape)
+
+import os
+from utils.loaders import load_mnist
+from models.VAE_CONV_D1 import VariationalAutoencoder
+import numpy as np
+
+# 실행 매개변수
+SECTION = 'vae_conv_d1'
+RUN_ID = '0001'
+DATA_NAME = 'res'
+RUN_FOLDER = 'run/{}/'.format(SECTION)
+RUN_FOLDER += '_'.join([RUN_ID, DATA_NAME])
+
+if not os.path.exists(RUN_FOLDER):
+    os.mkdir(RUN_FOLDER)
+    os.mkdir(os.path.join(RUN_FOLDER, 'viz'))
+    os.mkdir(os.path.join(RUN_FOLDER, 'images'))
+    os.mkdir(os.path.join(RUN_FOLDER, 'weights'))
+
+MODE = 'build'
+
+# CONV_1D를 합성곱 신경망으로 사용하는 VAE, 압축 인자(z_dim)=# of extracted features
+VAE = VariationalAutoencoder(
+    input_dim = (24,4)
+    , encoder_conv_filters = [32,64,64,64]
+    , encoder_conv_kernel_size = [3,3,3,3]
+    # , encoder_conv_strides = [1,2,2,1]
+    , encoder_conv_strides = [1,1,2,2]
+    , decoder_conv_t_filters = [64,64,32,1]
+    , decoder_conv_t_kernel_size = [3,3,3,3]
+    # , decoder_conv_t_strides = [1,2,2,1]
+    , decoder_conv_t_strides=[1,1,2,2]
+    , z_dim = 2
+)
+
+if MODE == 'build':
+    VAE.save(RUN_FOLDER)
+else:
+    VAE.load_weights(os.path.join(RUN_FOLDER, 'weights/weights.h5'))
+
+# hyper-parameter 설정
+LEARNING_RATE = 0.0005
+R_LOSS_FACTOR = 1000
+
+BATCH_SIZE = 32
+EPOCHS = 500
+PRINT_EVERY_N_BATCHES = 100
+INITIAL_EPOCH = 0
+
+VAE.compile(LEARNING_RATE, R_LOSS_FACTOR)
+
+VAE.train(
+    x_train
+    , batch_size = BATCH_SIZE
+    , epochs = EPOCHS
+    , run_folder = RUN_FOLDER
+    , print_every_n_batches = PRINT_EVERY_N_BATCHES
+    , initial_epoch = INITIAL_EPOCH
+)
+
