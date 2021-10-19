728332292e44c317f428848e5e942a9017989a0e
AIPSE-GNU <s.hwangbo@gnu.ac.kr>
2021-10-19 17:24:25 +0900

    sample generation model-1

---
+++ 1. Sample Generation model/VAE/ex_load_data_002.py
@@ -0,0 +1,71 @@
+from numpy import mean
+from numpy import std
+from numpy import dstack
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+
+# 호출 순서:
+# load_dataset => load_dataset_group => load_group => load_file
+
+# load_dataset:
+# A. 최종 실행 함수 (input 인자는 따로 없음)
+# B. 최상위 폴더(here, RES)와 dataset 구분된 폴더(here, train or test) 접속 실행 함수인 load_dataset_group 포함
+
+# load_dataset_group:
+# A. 최상위 폴더와 dataset 구분된 폴더 (here, train or test) 호출을 통하여 최하위 폴더(here, RES) 및 파일들(csv) 접속
+
+# load_group:
+# A. 최상위 폴더부터 최하위 폴더까지의 경로와 개별 파일(csv) 연결을 filepath 형태로 load_file의 input 인자로 넘김
+# B. for 문을 통하여 각각의 개별 파일(csv)들에 대하여 load_file을 실행하고 결과 값들은 모두 append 한 후 dstack을 통하여 3차원 배열 형성
+
+# load_file:
+# A. 최상위 폴더부터 최하위 폴더까지의 경로와 개별 파일(csv)에 접속하여 실제 값들을 호출
+# B. 이를 다시 위 함수들의 호출 순서 역으로 전파
+
+
+# load a single file as a numpy array
+# 개별 파일에 접속하여 값들을 배출
+def load_file(filepath):
+    dataframe = read_csv(filepath, header=None)
+    return dataframe.values[1:, 1:]
+
+
+# load a list of files and return as a 3d numpy array
+# 전체 파일들 내의 개별 파일에 접속하여 호출된 값들을 리스트에 append 한 후 하나의 3차원 배열로 배출
+def load_group(filenames, prefix=''):
+    loaded = list()
+    for name in filenames:
+        data = load_file(prefix + name)
+        loaded.append(data)
+    # stack group so that features are the 3rd dimension
+    # (sample 개수, 시간, feature)의 3차원 배열 형성
+    loaded = dstack(loaded)
+    return loaded
+
+
+# load a dataset group, such as train or test
+# 데이터 세트(train 혹은 test) 폴더에 대응되는 경로(filepath) 및 폴더 내 전체 파일들(filename) 이름을 load_group에 전달
+def load_dataset_group(group, prefix=''):
+    filepath = prefix + group + '/RES_YB/'
+
+    filenames = list()
+    # RE demand/solar
+    filenames += ['raw_data_jeju_solar(2030).csv', 'raw_data_jeju_wind(2030).csv', 'raw_data_land_solar(2030).csv', 'raw_data_land_wind(2030).csv']
+    # load input data
+    X = load_group(filenames, filepath)
+    return X
+
+
+# load the dataset, returns train and test X and y elements
+# sampling의 경우 test data는 사용되지 않기에 test_size=0 으로 설정 
+def load_dataset(prefix=''):
+    # load all train
+    total_data = load_dataset_group('train', prefix + 'RES/')
+    return total_data
+    # trainX, testX = train_test_split(total_data, test_size=0, random_state=123)
+    # return trainX, testX
+
+
+total_data = load_dataset()
+print(total_data)
---
+++ 1. Sample Generation model/VAE/jeju_solar_sample_distribution.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import random
+
+df = pd.read_csv("./RES/vae_generative_test_jeju_solar_01.csv", header=None)
+# print(df)
+
+ind = np.arange(365*50)
+random.seed(42)
+random.shuffle(ind)
+
+df_100 = np.empty((1, 24), object)
+df_100 = pd.DataFrame(df_100)
+# df_to = np.empty((1,24), object)
+# df_to = pd.DataFrame(df_to)
+k = 0
+for i in range(365):
+    for j in range(50):
+        globals()['df{}'.format(i)] = df.iloc[k, :]
+        df_100 = df_100.append(globals()['df{}'.format(i)], ignore_index=True)
+        k += 1
+        print(k)
+    df_100 = df_100[1:]
+    if i == 0:
+        df_to = df_100
+        df_100 = np.empty((1, 24), object)
+        df_100 = pd.DataFrame(df_100)
+    else:
+        df_to = pd.concat([df_to, df_100], axis=1)
+        df_100 = np.empty((1, 24), object)
+        df_100 = pd.DataFrame(df_100)
+
+print(df_to)
+df_to.to_csv("sampling/jeju_solar_rs42/sampling_jeju_solar_50.csv", header=None, index=False)
